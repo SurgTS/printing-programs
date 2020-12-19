b20a39c820872837bf2e58393b790445011c7ccd
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2020-12-19 15:34:01 +0100

    Print TOpcfgRt97nO for 138 times, in java

---
+++ java/TOpcfgRt97nO.java
@@ -0,0 +1,8 @@
+public class TOpcfgRt97nO {
+    public static void main(String[] args) {
+        int x = 138;
+        while (x-- > 0) {
+            System.out.println("TOpcfgRt97nO");
+        }
+    }
+}
