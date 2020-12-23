7094196b054899a12dc61fd39a22f0ec7336f09b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2020-12-23 04:59:30 +0100

    Print NHlkK9eysTtG for 141 times, in java

---
+++ java/NHlkK9eysTtG.java
@@ -0,0 +1,8 @@
+public class NHlkK9eysTtG {
+    public static void main(String[] args) {
+        int x = 141;
+        while (x-- > 0) {
+            System.out.println("NHlkK9eysTtG");
+        }
+    }
+}
