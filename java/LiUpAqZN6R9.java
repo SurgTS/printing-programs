55c17498c35c7ea2cd59e0e68c092d3938d5dbb4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2020-12-30 14:10:58 +0100

    Print LiUpAqZN6R9 for 44 times, in java

---
+++ java/LiUpAqZN6R9.java
@@ -0,0 +1,8 @@
+public class LiUpAqZN6R9 {
+    public static void main(String[] args) {
+        int x = 44;
+        while (x-- > 0) {
+            System.out.println("LiUpAqZN6R9");
+        }
+    }
+}
