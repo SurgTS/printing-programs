07d650798b3b7ecd83b3c7633a1897ecb01c36f9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-01-08 16:27:40 +0100

    Print KaKxsn8ngGNGx for 62 times, in java

---
+++ java/KaKxsn8ngGNGx.java
@@ -0,0 +1,8 @@
+public class KaKxsn8ngGNGx {
+    public static void main(String[] args) {
+        int x = 62;
+        while (x-- > 0) {
+            System.out.println("KaKxsn8ngGNGx");
+        }
+    }
+}
