049d68d4730fd6286768ba9b90962cb605b1b36f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-01-10 22:13:41 +0100

    Print Mdods88tcVTjg for 206 times, in java

---
+++ java/Mdods88tcVTjg.java
@@ -0,0 +1,8 @@
+public class Mdods88tcVTjg {
+    public static void main(String[] args) {
+        int x = 206;
+        while (x-- > 0) {
+            System.out.println("Mdods88tcVTjg");
+        }
+    }
+}
