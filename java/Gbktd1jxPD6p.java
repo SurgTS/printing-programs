a33270955d52fa8f69502142d2a6e06dcc726de5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-01-18 01:16:40 +0100

    Print Gbktd1jxPD6p for 107 times, in java

---
+++ java/Gbktd1jxPD6p.java
@@ -0,0 +1,8 @@
+public class Gbktd1jxPD6p {
+    public static void main(String[] args) {
+        int x = 107;
+        while (x-- > 0) {
+            System.out.println("Gbktd1jxPD6p");
+        }
+    }
+}
