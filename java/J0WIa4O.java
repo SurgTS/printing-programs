e6109665e1d3d85124a46036492d4468751f7550
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-01-19 22:47:44 +0100

    Print J0WIa4O for 192 times, in java

---
+++ java/J0WIa4O.java
@@ -0,0 +1,8 @@
+public class J0WIa4O {
+    public static void main(String[] args) {
+        int x = 192;
+        while (x-- > 0) {
+            System.out.println("J0WIa4O");
+        }
+    }
+}
