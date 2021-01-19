6aa7d045cd3c8b1a2e5a55257fbb6d2c7b680572
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-01-19 15:52:01 +0100

    Print AyANVx6jo for 17 times, in cpp

---
+++ cpp/AyANVx6jo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main() {
+    const auto msgCnt = 17;
+    const std::string msg = "AyANVx6jo";
+    for (int i = 0; i < msgCnt; ++i) {
+        std::foreach(msg.cbegin(), msg.cend(), [](const char& c) {
+            std::cout << c;
+        });
+        std::cout << std::endl;
+    }
+    return 0;
+}
