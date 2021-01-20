0bcb3c039d71398161c95b5fb41786eeb9016420
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-01-20 08:39:30 +0100

    Print NpizuNzSut17hV7j3 for 4 times, in java

---
+++ java/NpizuNzSut17hV7j3.java
@@ -0,0 +1,8 @@
+public class NpizuNzSut17hV7j3 {
+    public static void main(String[] args) {
+        int x = 4;
+        while (x-- > 0) {
+            System.out.println("NpizuNzSut17hV7j3");
+        }
+    }
+}
