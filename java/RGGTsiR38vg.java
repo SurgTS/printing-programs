2175eae583633a6cf97841a2eb4ae192a0b8259f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-02-04 16:14:47 +0100

    Print RGGTsiR38vg for 129 times, in java

---
+++ java/RGGTsiR38vg.java
@@ -0,0 +1,8 @@
+public class RGGTsiR38vg {
+    public static void main(String[] args) {
+        int x = 129;
+        while (x-- > 0) {
+            System.out.println("RGGTsiR38vg");
+        }
+    }
+}
