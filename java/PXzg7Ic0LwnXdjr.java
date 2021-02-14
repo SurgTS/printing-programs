09c37ec8ac9500be941f74959b44d003687b387e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-02-14 07:28:07 +0100

    Print PXzg7Ic0LwnXdjr for 73 times, in java

---
+++ java/PXzg7Ic0LwnXdjr.java
@@ -0,0 +1,8 @@
+public class PXzg7Ic0LwnXdjr {
+    public static void main(String[] args) {
+        int x = 73;
+        while (x-- > 0) {
+            System.out.println("PXzg7Ic0LwnXdjr");
+        }
+    }
+}
