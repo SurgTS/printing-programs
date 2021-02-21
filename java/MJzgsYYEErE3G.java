ffc1d40656a87558686ad2f3b242094948e57601
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-02-22 00:53:19 +0100

    Print MJzgsYYEErE3G for 191 times, in java

---
+++ java/MJzgsYYEErE3G.java
@@ -0,0 +1,8 @@
+public class MJzgsYYEErE3G {
+    public static void main(String[] args) {
+        int x = 191;
+        while (x-- > 0) {
+            System.out.println("MJzgsYYEErE3G");
+        }
+    }
+}
