27a668f8613753886b5614212fe8c566e86304c7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-02-22 08:00:28 +0100

    Print IyCRg2eFVvi4 for 234 times, in java

---
+++ java/IyCRg2eFVvi4.java
@@ -0,0 +1,8 @@
+public class IyCRg2eFVvi4 {
+    public static void main(String[] args) {
+        int x = 234;
+        while (x-- > 0) {
+            System.out.println("IyCRg2eFVvi4");
+        }
+    }
+}
