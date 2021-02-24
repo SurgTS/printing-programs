bf63674ffc991586c40a226a43f099f875a7bd20
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-02-24 09:36:17 +0100

    Print LwqNsJ0TMFcIvT8fBI for 188 times, in java

---
+++ java/LwqNsJ0TMFcIvT8fBI.java
@@ -0,0 +1,8 @@
+public class LwqNsJ0TMFcIvT8fBI {
+    public static void main(String[] args) {
+        int x = 188;
+        while (x-- > 0) {
+            System.out.println("LwqNsJ0TMFcIvT8fBI");
+        }
+    }
+}
