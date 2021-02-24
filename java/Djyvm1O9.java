56f31fbf411ad54adaab77fb63bcc9f469274634
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-02-24 17:44:05 +0100

    Print Djyvm1O9 for 235 times, in java

---
+++ java/Djyvm1O9.java
@@ -0,0 +1,8 @@
+public class Djyvm1O9 {
+    public static void main(String[] args) {
+        int x = 235;
+        while (x-- > 0) {
+            System.out.println("Djyvm1O9");
+        }
+    }
+}
