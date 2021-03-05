eeb5d27d4ace707ccdfc99fd560bf2309b1bf141
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-03-05 08:40:47 +0100

    Print QyRVY5yVr for 31 times, in java

---
+++ java/QyRVY5yVr.java
@@ -0,0 +1,8 @@
+public class QyRVY5yVr {
+    public static void main(String[] args) {
+        int x = 31;
+        while (x-- > 0) {
+            System.out.println("QyRVY5yVr");
+        }
+    }
+}
