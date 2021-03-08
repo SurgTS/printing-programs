f8a49e854de735a136b94f26d1000a82902bf694
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-03-08 12:37:56 +0100

    Print DRwI013Ke7Sd for 144 times, in java

---
+++ java/DRwI013Ke7Sd.java
@@ -0,0 +1,8 @@
+public class DRwI013Ke7Sd {
+    public static void main(String[] args) {
+        int x = 144;
+        while (x-- > 0) {
+            System.out.println("DRwI013Ke7Sd");
+        }
+    }
+}
