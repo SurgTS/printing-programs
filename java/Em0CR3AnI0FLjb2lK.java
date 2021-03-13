ee623ae329712be62a543f9e5d3d0e28cf366983
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-03-13 10:42:27 +0100

    Print Em0CR3AnI0FLjb2lK for 219 times, in java

---
+++ java/Em0CR3AnI0FLjb2lK.java
@@ -0,0 +1,8 @@
+public class Em0CR3AnI0FLjb2lK {
+    public static void main(String[] args) {
+        int x = 219;
+        while (x-- > 0) {
+            System.out.println("Em0CR3AnI0FLjb2lK");
+        }
+    }
+}
