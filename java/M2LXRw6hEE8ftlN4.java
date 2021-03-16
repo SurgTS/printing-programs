ae8dd401842e275c4d4f139ba842453271da1b09
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-03-16 21:33:11 +0100

    Print M2LXRw6hEE8ftlN4 for 162 times, in java

---
+++ java/M2LXRw6hEE8ftlN4.java
@@ -0,0 +1,8 @@
+public class M2LXRw6hEE8ftlN4 {
+    public static void main(String[] args) {
+        int x = 162;
+        while (x-- > 0) {
+            System.out.println("M2LXRw6hEE8ftlN4");
+        }
+    }
+}
