13c9d4639498146d634e67432ed4818f71ec9708
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-03-20 12:07:18 +0100

    Print XmDydWPCYez3XkQ for 119 times, in java

---
+++ java/XmDydWPCYez3XkQ.java
@@ -0,0 +1,8 @@
+public class XmDydWPCYez3XkQ {
+    public static void main(String[] args) {
+        int x = 119;
+        while (x-- > 0) {
+            System.out.println("XmDydWPCYez3XkQ");
+        }
+    }
+}
