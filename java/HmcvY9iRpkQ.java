dd3256534675438d3e7695617bb74a0ccbe224d7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-03-22 12:29:25 +0100

    Print HmcvY9iRpkQ for 239 times, in java

---
+++ java/HmcvY9iRpkQ.java
@@ -0,0 +1,8 @@
+public class HmcvY9iRpkQ {
+    public static void main(String[] args) {
+        int x = 239;
+        while (x-- > 0) {
+            System.out.println("HmcvY9iRpkQ");
+        }
+    }
+}
