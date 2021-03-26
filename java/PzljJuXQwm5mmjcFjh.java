9bdd12339ef1811a636557ba6ed96a05ace74ae1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-03-26 16:05:10 +0100

    Print PzljJuXQwm5mmjcFjh for 150 times, in java

---
+++ java/PzljJuXQwm5mmjcFjh.java
@@ -0,0 +1,8 @@
+public class PzljJuXQwm5mmjcFjh {
+    public static void main(String[] args) {
+        int x = 150;
+        while (x-- > 0) {
+            System.out.println("PzljJuXQwm5mmjcFjh");
+        }
+    }
+}
