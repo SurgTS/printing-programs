4a69f3313824a243c09ae2b37e38e578e6b8f539
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-03-28 07:27:51 +0200

    Print NfLKkJ7J for 174 times, in java

---
+++ java/NfLKkJ7J.java
@@ -0,0 +1,8 @@
+public class NfLKkJ7J {
+    public static void main(String[] args) {
+        int x = 174;
+        while (x-- > 0) {
+            System.out.println("NfLKkJ7J");
+        }
+    }
+}
