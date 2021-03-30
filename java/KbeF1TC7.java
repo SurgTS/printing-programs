f0e8845ab9c2b2714961302fd522de488616c12d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-03-30 03:01:54 +0200

    Print KbeF1TC7 for 161 times, in java

---
+++ java/KbeF1TC7.java
@@ -0,0 +1,8 @@
+public class KbeF1TC7 {
+    public static void main(String[] args) {
+        int x = 161;
+        while (x-- > 0) {
+            System.out.println("KbeF1TC7");
+        }
+    }
+}
