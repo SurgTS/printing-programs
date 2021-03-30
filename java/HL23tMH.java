4523cc74489e6d3a3a621ca30586db0ad4a478b4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-03-30 11:09:02 +0200

    Print HL23tMH for 112 times, in java

---
+++ java/HL23tMH.java
@@ -0,0 +1,8 @@
+public class HL23tMH {
+    public static void main(String[] args) {
+        int x = 112;
+        while (x-- > 0) {
+            System.out.println("HL23tMH");
+        }
+    }
+}
