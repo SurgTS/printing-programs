13a8680116d3b9512747decb04b24c5021efe423
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-04-11 17:57:41 +0200

    Print Yp3LR3HshIPQ for 111 times, in java

---
+++ java/Yp3LR3HshIPQ.java
@@ -0,0 +1,8 @@
+public class Yp3LR3HshIPQ {
+    public static void main(String[] args) {
+        int x = 111;
+        while (x-- > 0) {
+            System.out.println("Yp3LR3HshIPQ");
+        }
+    }
+}
