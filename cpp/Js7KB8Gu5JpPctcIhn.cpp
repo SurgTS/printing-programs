ad4f85a44bb09c3b591966d0c0afd3bab17bdb0c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-04-21 08:58:34 +0200

    Print Js7KB8Gu5JpPctcIhn for 130 times, in cpp

---
+++ cpp/Js7KB8Gu5JpPctcIhn.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main() {
+    const auto msgCnt = 130;
+    const std::string msg = "Js7KB8Gu5JpPctcIhn";
+    for (int i = 0; i < msgCnt; ++i) {
+        std::foreach(msg.cbegin(), msg.cend(), [](const char& c) {
+            std::cout << c;
+        });
+        std::cout << std::endl;
+    }
+    return 0;
+}
