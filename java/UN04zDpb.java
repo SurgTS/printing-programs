d2f89fe0d47da599f0a9add7f68d048154134cec
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-04-28 14:52:41 +0200

    Print UN04zDpb for 37 times, in java

---
+++ java/UN04zDpb.java
@@ -0,0 +1,8 @@
+public class UN04zDpb {
+    public static void main(String[] args) {
+        int x = 37;
+        while (x-- > 0) {
+            System.out.println("UN04zDpb");
+        }
+    }
+}
