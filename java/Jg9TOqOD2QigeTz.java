49e598386a2b2cde6b528ada7643ae36fa16fd91
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-04-29 23:45:15 +0200

    Print Jg9TOqOD2QigeTz for 245 times, in java

---
+++ java/Jg9TOqOD2QigeTz.java
@@ -0,0 +1,8 @@
+public class Jg9TOqOD2QigeTz {
+    public static void main(String[] args) {
+        int x = 245;
+        while (x-- > 0) {
+            System.out.println("Jg9TOqOD2QigeTz");
+        }
+    }
+}
