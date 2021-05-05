2c91994987385de742e4aecdc71473efc45c31ce
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-05-05 22:55:45 +0200

    Print QeJbkZAXTtV1 for 170 times, in java

---
+++ java/QeJbkZAXTtV1.java
@@ -0,0 +1,8 @@
+public class QeJbkZAXTtV1 {
+    public static void main(String[] args) {
+        int x = 170;
+        while (x-- > 0) {
+            System.out.println("QeJbkZAXTtV1");
+        }
+    }
+}
