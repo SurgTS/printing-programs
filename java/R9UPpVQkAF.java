86886f283d27c339128dbb3857586dcea2bb20f9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-05-09 19:33:08 +0200

    Print R9UPpVQkAF for 97 times, in java

---
+++ java/R9UPpVQkAF.java
@@ -0,0 +1,8 @@
+public class R9UPpVQkAF {
+    public static void main(String[] args) {
+        int x = 97;
+        while (x-- > 0) {
+            System.out.println("R9UPpVQkAF");
+        }
+    }
+}
