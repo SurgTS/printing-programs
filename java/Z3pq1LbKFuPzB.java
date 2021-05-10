f6d8f28f9014803bcec3ce798301add10f9041a7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-05-10 15:25:59 +0200

    Print Z3pq1LbKFuPzB for 236 times, in java

---
+++ java/Z3pq1LbKFuPzB.java
@@ -0,0 +1,8 @@
+public class Z3pq1LbKFuPzB {
+    public static void main(String[] args) {
+        int x = 236;
+        while (x-- > 0) {
+            System.out.println("Z3pq1LbKFuPzB");
+        }
+    }
+}
