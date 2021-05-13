e92b17af563a038993010041c087613d8e7007d5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-05-13 23:04:04 +0200

    Print FkXpl0X0P4JTC816 for 50 times, in java

---
+++ java/FkXpl0X0P4JTC816.java
@@ -0,0 +1,8 @@
+public class FkXpl0X0P4JTC816 {
+    public static void main(String[] args) {
+        int x = 50;
+        while (x-- > 0) {
+            System.out.println("FkXpl0X0P4JTC816");
+        }
+    }
+}
