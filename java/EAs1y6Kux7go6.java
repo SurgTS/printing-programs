85b39d6b4af4dab2dba63c7a4e3f9022bbf14136
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-05-13 05:15:02 +0200

    Print EAs1y6Kux7go6 for 82 times, in java

---
+++ java/EAs1y6Kux7go6.java
@@ -0,0 +1,8 @@
+public class EAs1y6Kux7go6 {
+    public static void main(String[] args) {
+        int x = 82;
+        while (x-- > 0) {
+            System.out.println("EAs1y6Kux7go6");
+        }
+    }
+}
