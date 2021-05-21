8100938eca57f27903b47ada3dff651638123363
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-05-21 02:46:22 +0200

    Print Lt3SbC461Y1Lfq9 for 220 times, in java

---
+++ java/Lt3SbC461Y1Lfq9.java
@@ -0,0 +1,8 @@
+public class Lt3SbC461Y1Lfq9 {
+    public static void main(String[] args) {
+        int x = 220;
+        while (x-- > 0) {
+            System.out.println("Lt3SbC461Y1Lfq9");
+        }
+    }
+}
