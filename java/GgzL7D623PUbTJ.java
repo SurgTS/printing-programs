64b0bf553cf13993cba88beaa64d859eee545e7f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-05-28 12:26:07 +0200

    Print GgzL7D623PUbTJ for 175 times, in java

---
+++ java/GgzL7D623PUbTJ.java
@@ -0,0 +1,8 @@
+public class GgzL7D623PUbTJ {
+    public static void main(String[] args) {
+        int x = 175;
+        while (x-- > 0) {
+            System.out.println("GgzL7D623PUbTJ");
+        }
+    }
+}
