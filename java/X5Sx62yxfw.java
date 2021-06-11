c9ccb0cb18265504d754d89d82135e63cbbdbdb8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-06-11 18:21:43 +0200

    Print X5Sx62yxfw for 48 times, in java

---
+++ java/X5Sx62yxfw.java
@@ -0,0 +1,8 @@
+public class X5Sx62yxfw {
+    public static void main(String[] args) {
+        int x = 48;
+        while (x-- > 0) {
+            System.out.println("X5Sx62yxfw");
+        }
+    }
+}
