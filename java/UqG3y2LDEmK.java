aca73f405b9b48016888913dbb59240f65dfdc58
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-06-30 10:14:06 +0200

    Print UqG3y2LDEmK for 6 times, in java

---
+++ java/UqG3y2LDEmK.java
@@ -0,0 +1,8 @@
+public class UqG3y2LDEmK {
+    public static void main(String[] args) {
+        int x = 6;
+        while (x-- > 0) {
+            System.out.println("UqG3y2LDEmK");
+        }
+    }
+}
