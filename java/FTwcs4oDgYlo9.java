5a45bb27740e37c4e81f2d968a6518d72fd2a68b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-07-03 03:25:50 +0200

    Print FTwcs4oDgYlo9 for 14 times, in java

---
+++ java/FTwcs4oDgYlo9.java
@@ -0,0 +1,8 @@
+public class FTwcs4oDgYlo9 {
+    public static void main(String[] args) {
+        int x = 14;
+        while (x-- > 0) {
+            System.out.println("FTwcs4oDgYlo9");
+        }
+    }
+}
