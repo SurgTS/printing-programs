9cc3a21a93deb6a8b10bed55406785a6703e0972
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-07-11 11:03:04 +0200

    Print MeWIAgJAxnWIasAf5 for 172 times, in java

---
+++ java/MeWIAgJAxnWIasAf5.java
@@ -0,0 +1,8 @@
+public class MeWIAgJAxnWIasAf5 {
+    public static void main(String[] args) {
+        int x = 172;
+        while (x-- > 0) {
+            System.out.println("MeWIAgJAxnWIasAf5");
+        }
+    }
+}
