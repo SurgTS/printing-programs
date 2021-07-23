3230008a83edbd097c2fecccb69ef0cb535b868c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-07-23 23:52:16 +0200

    Print Y7jlPrLOhVkWt for 66 times, in cpp

---
+++ cpp/Y7jlPrLOhVkWt.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main() {
+    const auto msgCnt = 66;
+    const std::string msg = "Y7jlPrLOhVkWt";
+    for (int i = 0; i < msgCnt; ++i) {
+        std::foreach(msg.cbegin(), msg.cend(), [](const char& c) {
+            std::cout << c;
+        });
+        std::cout << std::endl;
+    }
+    return 0;
+}
