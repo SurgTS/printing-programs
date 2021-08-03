3832fc2f0c85156ed548a719a701adb81d16c374
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-08-03 02:08:39 +0200

    Print IVaMewor for 133 times, in java

---
+++ java/IVaMewor.java
@@ -0,0 +1,8 @@
+public class IVaMewor {
+    public static void main(String[] args) {
+        int x = 133;
+        while (x-- > 0) {
+            System.out.println("IVaMewor");
+        }
+    }
+}
