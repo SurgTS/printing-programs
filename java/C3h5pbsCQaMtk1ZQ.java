1feb80697026395a9f800389531ae750d7e14826
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-08-11 06:43:42 +0200

    Print C3h5pbsCQaMtk1ZQ for 133 times, in java

---
+++ java/C3h5pbsCQaMtk1ZQ.java
@@ -0,0 +1,8 @@
+public class C3h5pbsCQaMtk1ZQ {
+    public static void main(String[] args) {
+        int x = 133;
+        while (x-- > 0) {
+            System.out.println("C3h5pbsCQaMtk1ZQ");
+        }
+    }
+}
