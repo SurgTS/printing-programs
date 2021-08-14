eba7f720818719518caa845935af84db2e7fa265
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-08-14 19:51:58 +0200

    Print C74noYMWPL9 for 96 times, in java

---
+++ java/C74noYMWPL9.java
@@ -0,0 +1,8 @@
+public class C74noYMWPL9 {
+    public static void main(String[] args) {
+        int x = 96;
+        while (x-- > 0) {
+            System.out.println("C74noYMWPL9");
+        }
+    }
+}
