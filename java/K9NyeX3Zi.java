61772e8e798da633155a37d4d1570979e7881236
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-08-18 07:39:56 +0200

    Print K9NyeX3Zi for 180 times, in java

---
+++ java/K9NyeX3Zi.java
@@ -0,0 +1,8 @@
+public class K9NyeX3Zi {
+    public static void main(String[] args) {
+        int x = 180;
+        while (x-- > 0) {
+            System.out.println("K9NyeX3Zi");
+        }
+    }
+}
