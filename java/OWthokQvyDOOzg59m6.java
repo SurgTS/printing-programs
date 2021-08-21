64ef8437f9f1c11efd4044a63d4d315a744cf707
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-08-21 07:41:59 +0200

    Print OWthokQvyDOOzg59m6 for 91 times, in java

---
+++ java/OWthokQvyDOOzg59m6.java
@@ -0,0 +1,8 @@
+public class OWthokQvyDOOzg59m6 {
+    public static void main(String[] args) {
+        int x = 91;
+        while (x-- > 0) {
+            System.out.println("OWthokQvyDOOzg59m6");
+        }
+    }
+}
