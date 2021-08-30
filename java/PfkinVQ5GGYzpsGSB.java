f8d3973fb38242ffb014e834f0e89fbd00e14dfa
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-08-30 02:13:41 +0200

    Print PfkinVQ5GGYzpsGSB for 132 times, in java

---
+++ java/PfkinVQ5GGYzpsGSB.java
@@ -0,0 +1,8 @@
+public class PfkinVQ5GGYzpsGSB {
+    public static void main(String[] args) {
+        int x = 132;
+        while (x-- > 0) {
+            System.out.println("PfkinVQ5GGYzpsGSB");
+        }
+    }
+}
