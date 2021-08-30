6a2ec12679b6b4bdb445f92eb3a626825d954859
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-08-31 01:59:08 +0200

    Print WRNUynQz1dkQzmyxIQ for 111 times, in java

---
+++ java/WRNUynQz1dkQzmyxIQ.java
@@ -0,0 +1,8 @@
+public class WRNUynQz1dkQzmyxIQ {
+    public static void main(String[] args) {
+        int x = 111;
+        while (x-- > 0) {
+            System.out.println("WRNUynQz1dkQzmyxIQ");
+        }
+    }
+}
