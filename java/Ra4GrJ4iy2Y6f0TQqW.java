c1c95343d099dece645cbe30124d24f71f0268e9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-09-08 00:59:10 +0200

    Print Ra4GrJ4iy2Y6f0TQqW for 218 times, in java

---
+++ java/Ra4GrJ4iy2Y6f0TQqW.java
@@ -0,0 +1,8 @@
+public class Ra4GrJ4iy2Y6f0TQqW {
+    public static void main(String[] args) {
+        int x = 218;
+        while (x-- > 0) {
+            System.out.println("Ra4GrJ4iy2Y6f0TQqW");
+        }
+    }
+}
