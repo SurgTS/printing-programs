555b7e0a8105b9b3a42d367f9ce7165cdb9665d1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-09-10 01:07:15 +0200

    Print J6wP1fAohV2kBPKPZL for 199 times, in java

---
+++ java/J6wP1fAohV2kBPKPZL.java
@@ -0,0 +1,8 @@
+public class J6wP1fAohV2kBPKPZL {
+    public static void main(String[] args) {
+        int x = 199;
+        while (x-- > 0) {
+            System.out.println("J6wP1fAohV2kBPKPZL");
+        }
+    }
+}
