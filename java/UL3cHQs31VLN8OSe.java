47f8fac6b9c7c9e1d135a11245cdc8d93eac48d8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-09-11 22:57:41 +0200

    Print UL3cHQs31VLN8OSe for 122 times, in java

---
+++ java/UL3cHQs31VLN8OSe.java
@@ -0,0 +1,8 @@
+public class UL3cHQs31VLN8OSe {
+    public static void main(String[] args) {
+        int x = 122;
+        while (x-- > 0) {
+            System.out.println("UL3cHQs31VLN8OSe");
+        }
+    }
+}
