b444d9557e188d068f285fe4d0789088429df4b5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-09-17 01:42:17 +0200

    Print A7bMb8LbsCx1PfEK0 for 155 times, in java

---
+++ java/A7bMb8LbsCx1PfEK0.java
@@ -0,0 +1,8 @@
+public class A7bMb8LbsCx1PfEK0 {
+    public static void main(String[] args) {
+        int x = 155;
+        while (x-- > 0) {
+            System.out.println("A7bMb8LbsCx1PfEK0");
+        }
+    }
+}
