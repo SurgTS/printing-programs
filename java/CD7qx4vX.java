84a7099bd76c151484e9a29468b1065834018f8f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-09-22 04:11:47 +0200

    Print CD7qx4vX for 157 times, in java

---
+++ java/CD7qx4vX.java
@@ -0,0 +1,8 @@
+public class CD7qx4vX {
+    public static void main(String[] args) {
+        int x = 157;
+        while (x-- > 0) {
+            System.out.println("CD7qx4vX");
+        }
+    }
+}
