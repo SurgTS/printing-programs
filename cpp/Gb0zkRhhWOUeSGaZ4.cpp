3b66ce04431c8201f81210485d1effca32d1e275
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-10-04 19:24:08 +0200

    Print Gb0zkRhhWOUeSGaZ4 for 56 times, in cpp

---
+++ cpp/Gb0zkRhhWOUeSGaZ4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main() {
+    const auto msgCnt = 56;
+    const std::string msg = "Gb0zkRhhWOUeSGaZ4";
+    for (int i = 0; i < msgCnt; ++i) {
+        std::foreach(msg.cbegin(), msg.cend(), [](const char& c) {
+            std::cout << c;
+        });
+        std::cout << std::endl;
+    }
+    return 0;
+}
