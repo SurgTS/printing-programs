9db053481f095829efc217518dea6605759f6397
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-10-16 03:50:26 +0200

    Print A4vBZWJe4A2TRjBvzGV for 108 times, in java

---
+++ java/A4vBZWJe4A2TRjBvzGV.java
@@ -0,0 +1,8 @@
+public class A4vBZWJe4A2TRjBvzGV {
+    public static void main(String[] args) {
+        int x = 108;
+        while (x-- > 0) {
+            System.out.println("A4vBZWJe4A2TRjBvzGV");
+        }
+    }
+}
