c57faca1a5e94f79c77dd9dc2be278e0e533a602
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-10-20 20:14:17 +0200

    Print EZMHLalaZzH46w1Z0t for 232 times, in java

---
+++ java/EZMHLalaZzH46w1Z0t.java
@@ -0,0 +1,8 @@
+public class EZMHLalaZzH46w1Z0t {
+    public static void main(String[] args) {
+        int x = 232;
+        while (x-- > 0) {
+            System.out.println("EZMHLalaZzH46w1Z0t");
+        }
+    }
+}
