7b98b71ed36cb76554b671dd6ac5bade4d9083b6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-10-26 19:20:32 +0200

    Print QtMLDj3GMOlODJ for 128 times, in java

---
+++ java/QtMLDj3GMOlODJ.java
@@ -0,0 +1,8 @@
+public class QtMLDj3GMOlODJ {
+    public static void main(String[] args) {
+        int x = 128;
+        while (x-- > 0) {
+            System.out.println("QtMLDj3GMOlODJ");
+        }
+    }
+}
