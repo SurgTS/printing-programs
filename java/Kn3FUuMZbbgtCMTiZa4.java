e8aa9a93b4c102bf9ab2b16198cf2e6d149db358
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-10-27 19:16:45 +0200

    Print Kn3FUuMZbbgtCMTiZa4 for 243 times, in java

---
+++ java/Kn3FUuMZbbgtCMTiZa4.java
@@ -0,0 +1,8 @@
+public class Kn3FUuMZbbgtCMTiZa4 {
+    public static void main(String[] args) {
+        int x = 243;
+        while (x-- > 0) {
+            System.out.println("Kn3FUuMZbbgtCMTiZa4");
+        }
+    }
+}
