01d948f1fed060e6f96bf67e840aaa75d92fbe6f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-10-27 10:24:19 +0200

    Print UnBreHioeadw8U for 119 times, in java

---
+++ java/UnBreHioeadw8U.java
@@ -0,0 +1,8 @@
+public class UnBreHioeadw8U {
+    public static void main(String[] args) {
+        int x = 119;
+        while (x-- > 0) {
+            System.out.println("UnBreHioeadw8U");
+        }
+    }
+}
