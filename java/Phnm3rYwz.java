6d9ee3606426851c7cf53c05cc2010d9456df500
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-10-30 02:22:47 +0200

    Print Phnm3rYwz for 1 times, in java

---
+++ java/Phnm3rYwz.java
@@ -0,0 +1,8 @@
+public class Phnm3rYwz {
+    public static void main(String[] args) {
+        int x = 1;
+        while (x-- > 0) {
+            System.out.println("Phnm3rYwz");
+        }
+    }
+}
