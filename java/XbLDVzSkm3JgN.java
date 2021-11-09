2b4a66b903945919d2d4baef4028077fb4626d41
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-11-09 20:49:18 +0100

    Print XbLDVzSkm3JgN for 134 times, in java

---
+++ java/XbLDVzSkm3JgN.java
@@ -0,0 +1,8 @@
+public class XbLDVzSkm3JgN {
+    public static void main(String[] args) {
+        int x = 134;
+        while (x-- > 0) {
+            System.out.println("XbLDVzSkm3JgN");
+        }
+    }
+}
