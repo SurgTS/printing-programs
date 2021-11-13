71eedb44b6897fd173b2c4fd2c028b65a5bd3076
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-11-13 15:02:48 +0100

    Print AuuQhL3C for 122 times, in java

---
+++ java/AuuQhL3C.java
@@ -0,0 +1,8 @@
+public class AuuQhL3C {
+    public static void main(String[] args) {
+        int x = 122;
+        while (x-- > 0) {
+            System.out.println("AuuQhL3C");
+        }
+    }
+}
