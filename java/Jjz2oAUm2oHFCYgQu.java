52fb5eda19b6f1c9cb98a8e9ef4c66b750e35b07
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-11-17 15:10:18 +0100

    Print Jjz2oAUm2oHFCYgQu for 96 times, in java

---
+++ java/Jjz2oAUm2oHFCYgQu.java
@@ -0,0 +1,8 @@
+public class Jjz2oAUm2oHFCYgQu {
+    public static void main(String[] args) {
+        int x = 96;
+        while (x-- > 0) {
+            System.out.println("Jjz2oAUm2oHFCYgQu");
+        }
+    }
+}
