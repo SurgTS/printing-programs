c040633e561a2f8f00eea94fa615987004a6f9a6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-11-17 22:34:50 +0100

    Print ZAlOCEBV for 5 times, in cpp

---
+++ cpp/ZAlOCEBV.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main() {
+    const auto msgCnt = 5;
+    const std::string msg = "ZAlOCEBV";
+    for (int i = 0; i < msgCnt; ++i) {
+        std::foreach(msg.cbegin(), msg.cend(), [](const char& c) {
+            std::cout << c;
+        });
+        std::cout << std::endl;
+    }
+    return 0;
+}
