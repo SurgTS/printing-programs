5df7f1f41e48fa49b795e167930251009f66869c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-11-18 07:26:03 +0100

    Print MIuskTZSumSD for 159 times, in java

---
+++ java/MIuskTZSumSD.java
@@ -0,0 +1,8 @@
+public class MIuskTZSumSD {
+    public static void main(String[] args) {
+        int x = 159;
+        while (x-- > 0) {
+            System.out.println("MIuskTZSumSD");
+        }
+    }
+}
