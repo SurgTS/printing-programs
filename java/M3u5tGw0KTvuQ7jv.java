fc0531008f6aabf6d1af5f95930d22dcb2cf4c11
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-11-19 19:12:35 +0100

    Print M3u5tGw0KTvuQ7jv for 75 times, in java

---
+++ java/M3u5tGw0KTvuQ7jv.java
@@ -0,0 +1,8 @@
+public class M3u5tGw0KTvuQ7jv {
+    public static void main(String[] args) {
+        int x = 75;
+        while (x-- > 0) {
+            System.out.println("M3u5tGw0KTvuQ7jv");
+        }
+    }
+}
