32591deaa0e733e3a27dc06ee74c7e1ac9e6efa5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-11-21 22:30:05 +0100

    Print Rfol3aFF for 190 times, in java

---
+++ java/Rfol3aFF.java
@@ -0,0 +1,8 @@
+public class Rfol3aFF {
+    public static void main(String[] args) {
+        int x = 190;
+        while (x-- > 0) {
+            System.out.println("Rfol3aFF");
+        }
+    }
+}
