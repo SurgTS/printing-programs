c6e36d19deec9e64c19ee1b9386fcda28c7076c3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-11-22 05:54:28 +0100

    Print TidCFLTmU4fUJ for 48 times, in java

---
+++ java/TidCFLTmU4fUJ.java
@@ -0,0 +1,8 @@
+public class TidCFLTmU4fUJ {
+    public static void main(String[] args) {
+        int x = 48;
+        while (x-- > 0) {
+            System.out.println("TidCFLTmU4fUJ");
+        }
+    }
+}
