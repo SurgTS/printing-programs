5dc511be8b4a21ee6a08b124951c885ddcba1a6c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-11-26 22:10:51 +0100

    Print BJlj7KVjdYpWUC6 for 170 times, in java

---
+++ java/BJlj7KVjdYpWUC6.java
@@ -0,0 +1,8 @@
+public class BJlj7KVjdYpWUC6 {
+    public static void main(String[] args) {
+        int x = 170;
+        while (x-- > 0) {
+            System.out.println("BJlj7KVjdYpWUC6");
+        }
+    }
+}
