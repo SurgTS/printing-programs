9f2418dc5f7d4ae108044cf73c3d1fa4fc7c83aa
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-12-12 21:52:04 +0100

    Print SVK0aew2s3DX for 108 times, in java

---
+++ java/SVK0aew2s3DX.java
@@ -0,0 +1,8 @@
+public class SVK0aew2s3DX {
+    public static void main(String[] args) {
+        int x = 108;
+        while (x-- > 0) {
+            System.out.println("SVK0aew2s3DX");
+        }
+    }
+}
