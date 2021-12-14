cf004045ce3244105e8b23fce91c8c5c71847b09
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-12-14 19:10:44 +0100

    Print BANSOLrxq for 51 times, in java

---
+++ java/BANSOLrxq.java
@@ -0,0 +1,8 @@
+public class BANSOLrxq {
+    public static void main(String[] args) {
+        int x = 51;
+        while (x-- > 0) {
+            System.out.println("BANSOLrxq");
+        }
+    }
+}
