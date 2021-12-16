25ab500d8f5589bdba8ab981209ec1204a42262e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-12-16 13:19:18 +0100

    Print MOctYvbdlblJ2 for 156 times, in java

---
+++ java/MOctYvbdlblJ2.java
@@ -0,0 +1,8 @@
+public class MOctYvbdlblJ2 {
+    public static void main(String[] args) {
+        int x = 156;
+        while (x-- > 0) {
+            System.out.println("MOctYvbdlblJ2");
+        }
+    }
+}
