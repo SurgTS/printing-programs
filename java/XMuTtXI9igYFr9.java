9b1470c308298f82a32e2dd34842e2a9d3b8f635
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-12-25 05:20:36 +0100

    Print XMuTtXI9igYFr9 for 74 times, in java

---
+++ java/XMuTtXI9igYFr9.java
@@ -0,0 +1,8 @@
+public class XMuTtXI9igYFr9 {
+    public static void main(String[] args) {
+        int x = 74;
+        while (x-- > 0) {
+            System.out.println("XMuTtXI9igYFr9");
+        }
+    }
+}
