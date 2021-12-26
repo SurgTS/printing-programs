6a48dad5f296ae32bf6961de78813afda565c282
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2021-12-26 06:42:25 +0100

    Print IV5rqLHd3ss7nsx for 44 times, in java

---
+++ java/IV5rqLHd3ss7nsx.java
@@ -0,0 +1,8 @@
+public class IV5rqLHd3ss7nsx {
+    public static void main(String[] args) {
+        int x = 44;
+        while (x-- > 0) {
+            System.out.println("IV5rqLHd3ss7nsx");
+        }
+    }
+}
