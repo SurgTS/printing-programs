9287fa60be896c337352ea8283385b93852b6336
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-01-07 01:39:31 +0100

    Print NU2yicqupt for 20 times, in java

---
+++ java/NU2yicqupt.java
@@ -0,0 +1,8 @@
+public class NU2yicqupt {
+    public static void main(String[] args) {
+        int x = 20;
+        while (x-- > 0) {
+            System.out.println("NU2yicqupt");
+        }
+    }
+}
