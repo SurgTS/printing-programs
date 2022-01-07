8202126e68ab9b01f8336c888c2d2f2c36face24
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-01-07 16:09:54 +0100

    Print GYHzLsHM for 28 times, in java

---
+++ java/GYHzLsHM.java
@@ -0,0 +1,8 @@
+public class GYHzLsHM {
+    public static void main(String[] args) {
+        int x = 28;
+        while (x-- > 0) {
+            System.out.println("GYHzLsHM");
+        }
+    }
+}
