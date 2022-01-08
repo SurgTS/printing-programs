d460eae42b1597cb5f75de51837b91b37ef7a5ca
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-01-08 13:40:48 +0100

    Print XTXqGQAHXppnqOud for 242 times, in java

---
+++ java/XTXqGQAHXppnqOud.java
@@ -0,0 +1,8 @@
+public class XTXqGQAHXppnqOud {
+    public static void main(String[] args) {
+        int x = 242;
+        while (x-- > 0) {
+            System.out.println("XTXqGQAHXppnqOud");
+        }
+    }
+}
