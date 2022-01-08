ea93ed7cc3a2563f086b428a46f52a91a24e9fc8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-01-08 20:32:00 +0100

    Print JYFOb26 for 206 times, in java

---
+++ java/JYFOb26.java
@@ -0,0 +1,8 @@
+public class JYFOb26 {
+    public static void main(String[] args) {
+        int x = 206;
+        while (x-- > 0) {
+            System.out.println("JYFOb26");
+        }
+    }
+}
