4e3359de5db295c4396a00c8a2e3339f428e28a7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-01-14 19:23:38 +0100

    Print HsEbeyCUCGNDJYM5A0 for 34 times, in java

---
+++ java/HsEbeyCUCGNDJYM5A0.java
@@ -0,0 +1,8 @@
+public class HsEbeyCUCGNDJYM5A0 {
+    public static void main(String[] args) {
+        int x = 34;
+        while (x-- > 0) {
+            System.out.println("HsEbeyCUCGNDJYM5A0");
+        }
+    }
+}
