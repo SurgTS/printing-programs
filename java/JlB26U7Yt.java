460a5e6b8b4e578fa234de877c94d6fb770a43d8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-01-19 01:44:51 +0100

    Print JlB26U7Yt for 236 times, in java

---
+++ java/JlB26U7Yt.java
@@ -0,0 +1,8 @@
+public class JlB26U7Yt {
+    public static void main(String[] args) {
+        int x = 236;
+        while (x-- > 0) {
+            System.out.println("JlB26U7Yt");
+        }
+    }
+}
