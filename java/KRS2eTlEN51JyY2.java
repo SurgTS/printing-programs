3b60ad56181312f2e2c6deee74f9460de04bdc8c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-01-20 23:11:44 +0100

    Print KRS2eTlEN51JyY2 for 69 times, in java

---
+++ java/KRS2eTlEN51JyY2.java
@@ -0,0 +1,8 @@
+public class KRS2eTlEN51JyY2 {
+    public static void main(String[] args) {
+        int x = 69;
+        while (x-- > 0) {
+            System.out.println("KRS2eTlEN51JyY2");
+        }
+    }
+}
