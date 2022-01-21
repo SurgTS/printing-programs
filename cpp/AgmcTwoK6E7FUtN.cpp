306c16e978398417b6748eb4966ac4246061d41f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-01-21 16:39:58 +0100

    Print AgmcTwoK6E7FUtN for 111 times, in cpp

---
+++ cpp/AgmcTwoK6E7FUtN.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main() {
+    const auto msgCnt = 111;
+    const std::string msg = "AgmcTwoK6E7FUtN";
+    for (int i = 0; i < msgCnt; ++i) {
+        std::foreach(msg.cbegin(), msg.cend(), [](const char& c) {
+            std::cout << c;
+        });
+        std::cout << std::endl;
+    }
+    return 0;
+}
