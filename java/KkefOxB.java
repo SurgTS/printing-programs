b2bbf7a260384966f3b01f9b70a0ca3cad452507
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-01-28 00:39:28 +0100

    Print KkefOxB for 199 times, in java

---
+++ java/KkefOxB.java
@@ -0,0 +1,8 @@
+public class KkefOxB {
+    public static void main(String[] args) {
+        int x = 199;
+        while (x-- > 0) {
+            System.out.println("KkefOxB");
+        }
+    }
+}
