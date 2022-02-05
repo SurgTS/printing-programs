e4fa186216a36e7e69cc7dcdfbf7e722706155aa
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-02-05 20:09:31 +0100

    Print NiaLAfTZD7wv for 71 times, in java

---
+++ java/NiaLAfTZD7wv.java
@@ -0,0 +1,8 @@
+public class NiaLAfTZD7wv {
+    public static void main(String[] args) {
+        int x = 71;
+        while (x-- > 0) {
+            System.out.println("NiaLAfTZD7wv");
+        }
+    }
+}
