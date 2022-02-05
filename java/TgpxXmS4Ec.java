2f7ee5eaea007d19720b7cfccdc0203c7ebbb861
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-02-05 10:30:54 +0100

    Print TgpxXmS4Ec for 54 times, in java

---
+++ java/TgpxXmS4Ec.java
@@ -0,0 +1,8 @@
+public class TgpxXmS4Ec {
+    public static void main(String[] args) {
+        int x = 54;
+        while (x-- > 0) {
+            System.out.println("TgpxXmS4Ec");
+        }
+    }
+}
