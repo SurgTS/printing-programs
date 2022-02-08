2ca43148f5f6723b14d6bea93ab23630b4bb9419
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-02-08 10:25:41 +0100

    Print IamozZ00huuPO for 0 times, in java

---
+++ java/IamozZ00huuPO.java
@@ -0,0 +1,8 @@
+public class IamozZ00huuPO {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x-- > 0) {
+            System.out.println("IamozZ00huuPO");
+        }
+    }
+}
