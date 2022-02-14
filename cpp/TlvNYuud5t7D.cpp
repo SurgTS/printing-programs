8b0b2ec829287ac27930aa4457199c276cff5829
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-02-14 04:30:37 +0100

    Print TlvNYuud5t7D for 61 times, in cpp

---
+++ cpp/TlvNYuud5t7D.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main() {
+    const auto msgCnt = 61;
+    const std::string msg = "TlvNYuud5t7D";
+    for (int i = 0; i < msgCnt; ++i) {
+        std::foreach(msg.cbegin(), msg.cend(), [](const char& c) {
+            std::cout << c;
+        });
+        std::cout << std::endl;
+    }
+    return 0;
+}
