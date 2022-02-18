0f538e49b1ef4c91b428eeff1d23384841755595
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-02-18 14:42:28 +0100

    Print QghpQqz1 for 71 times, in java

---
+++ java/QghpQqz1.java
@@ -0,0 +1,8 @@
+public class QghpQqz1 {
+    public static void main(String[] args) {
+        int x = 71;
+        while (x-- > 0) {
+            System.out.println("QghpQqz1");
+        }
+    }
+}
