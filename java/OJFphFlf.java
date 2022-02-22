f81548c67f889e0715a6dbc78296333abc5314ea
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-02-22 14:16:03 +0100

    Print OJFphFlf for 175 times, in java

---
+++ java/OJFphFlf.java
@@ -0,0 +1,8 @@
+public class OJFphFlf {
+    public static void main(String[] args) {
+        int x = 175;
+        while (x-- > 0) {
+            System.out.println("OJFphFlf");
+        }
+    }
+}
