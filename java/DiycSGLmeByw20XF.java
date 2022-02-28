f383a469c5a1835e0f83b351cb538c9984c22d7d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-02-28 13:12:23 +0100

    Print DiycSGLmeByw20XF for 189 times, in java

---
+++ java/DiycSGLmeByw20XF.java
@@ -0,0 +1,8 @@
+public class DiycSGLmeByw20XF {
+    public static void main(String[] args) {
+        int x = 189;
+        while (x-- > 0) {
+            System.out.println("DiycSGLmeByw20XF");
+        }
+    }
+}
