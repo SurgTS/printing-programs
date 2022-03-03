2b68642f2351854caf52cd0802311fe438681a45
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-03 02:01:44 +0100

    Print DSbqx3BFOO for 118 times, in java

---
+++ java/DSbqx3BFOO.java
@@ -0,0 +1,8 @@
+public class DSbqx3BFOO {
+    public static void main(String[] args) {
+        int x = 118;
+        while (x-- > 0) {
+            System.out.println("DSbqx3BFOO");
+        }
+    }
+}
