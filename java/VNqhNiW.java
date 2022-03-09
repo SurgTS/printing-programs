34e2bee779e3027150a9f1db49767b28a948578c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-09 21:27:53 +0100

    Print VNqhNiW for 112 times, in java

---
+++ java/VNqhNiW.java
@@ -0,0 +1,8 @@
+public class VNqhNiW {
+    public static void main(String[] args) {
+        int x = 112;
+        while (x-- > 0) {
+            System.out.println("VNqhNiW");
+        }
+    }
+}
