1cd042d8714b447d81764ebe3a13c10827f45caa
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-10 18:17:10 +0100

    Print FR47029EDG for 162 times, in java

---
+++ java/FR47029EDG.java
@@ -0,0 +1,8 @@
+public class FR47029EDG {
+    public static void main(String[] args) {
+        int x = 162;
+        while (x-- > 0) {
+            System.out.println("FR47029EDG");
+        }
+    }
+}
