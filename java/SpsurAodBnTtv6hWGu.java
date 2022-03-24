6e3d1e965436e8f586c825596808bfd711a6c343
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-24 01:25:25 +0100

    Print SpsurAodBnTtv6hWGu for 73 times, in java

---
+++ java/SpsurAodBnTtv6hWGu.java
@@ -0,0 +1,8 @@
+public class SpsurAodBnTtv6hWGu {
+    public static void main(String[] args) {
+        int x = 73;
+        while (x-- > 0) {
+            System.out.println("SpsurAodBnTtv6hWGu");
+        }
+    }
+}
