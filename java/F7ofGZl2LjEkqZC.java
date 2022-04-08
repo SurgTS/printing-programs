d4f89f5739a4854eafef1ee929e54b11b94584fd
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-08 19:37:19 +0200

    Print F7ofGZl2LjEkqZC for 216 times, in java

---
+++ java/F7ofGZl2LjEkqZC.java
@@ -0,0 +1,8 @@
+public class F7ofGZl2LjEkqZC {
+    public static void main(String[] args) {
+        int x = 216;
+        while (x-- > 0) {
+            System.out.println("F7ofGZl2LjEkqZC");
+        }
+    }
+}
