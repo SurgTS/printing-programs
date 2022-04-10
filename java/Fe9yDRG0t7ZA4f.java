9165a02926c23e8befcde1ebaec39c6cb4a455ea
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-10 12:49:51 +0200

    Print Fe9yDRG0t7ZA4f for 128 times, in java

---
+++ java/Fe9yDRG0t7ZA4f.java
@@ -0,0 +1,8 @@
+public class Fe9yDRG0t7ZA4f {
+    public static void main(String[] args) {
+        int x = 128;
+        while (x-- > 0) {
+            System.out.println("Fe9yDRG0t7ZA4f");
+        }
+    }
+}
