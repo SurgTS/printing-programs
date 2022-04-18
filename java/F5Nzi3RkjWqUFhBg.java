9bd72834c18be9d4a40a8b58562e1097761c012b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-18 11:06:28 +0200

    Print F5Nzi3RkjWqUFhBg for 0 times, in java

---
+++ java/F5Nzi3RkjWqUFhBg.java
@@ -0,0 +1,8 @@
+public class F5Nzi3RkjWqUFhBg {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x-- > 0) {
+            System.out.println("F5Nzi3RkjWqUFhBg");
+        }
+    }
+}
