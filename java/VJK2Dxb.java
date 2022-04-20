645f9249e9920ec874384a948ebe010293eb1649
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-20 16:14:33 +0200

    Print VJK2Dxb for 238 times, in java

---
+++ java/VJK2Dxb.java
@@ -0,0 +1,8 @@
+public class VJK2Dxb {
+    public static void main(String[] args) {
+        int x = 238;
+        while (x-- > 0) {
+            System.out.println("VJK2Dxb");
+        }
+    }
+}
