0bfcddae40d98eba6b8c3e4b962cd08cb9bcab32
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-22 00:48:18 +0200

    Print TJF3K7e for 220 times, in java

---
+++ java/TJF3K7e.java
@@ -0,0 +1,8 @@
+public class TJF3K7e {
+    public static void main(String[] args) {
+        int x = 220;
+        while (x-- > 0) {
+            System.out.println("TJF3K7e");
+        }
+    }
+}
